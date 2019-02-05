23aa7ae6733cd5cea8eeabf44c0ab5d29b662d4b
teamconvergence <46495005+teamconvergence@users.noreply.github.com>
2019-02-05 11:19:15 -0800

    Add files via upload

---
+++ frc/robot/RobotMap.java
@@ -0,0 +1,33 @@
+package frc.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+
+	public static int p_xbox1 = 0;
+
+	// DriveTrain Motors
+	public static int p_leftDrive1 = 10;
+	public static int p_leftDrive2 = 11;
+	public static int p_rightDrive1 = 12;
+	public static int p_rightDrive2 = 13;
+	public static int p_solenoidIn = 3;
+	public static int p_solenoidOut = 4;
+	public static int p_compressor = 5;
+	public static int p_limitleft = 0;
+	public static int p_limitright = 1;
+	public static int p_photoeye = 2;
+	public static int p_gyroscope = 0;
+	public static int p_liftmotor = 0;
+	public static int p_wristmotor = 0;
+	public static int p_encoderchannel1 = 0;
+	public static int p_encoderchannel2 = 0;
+	public static int p_secondencoderchannel1 = 2;
+	public static int p_secondencoderchannel2 = 2;
+
+}
+
